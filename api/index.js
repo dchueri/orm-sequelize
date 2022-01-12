dbec503028789b7f74789f59e41d905df996ef34
Diego <diegochueri@gmail.com>
2022-01-12 19:33:50 -0400

    Configurando o sequelize

--- api/index.js
+++ api/index.js
@@ -9,8 +9,7 @@ const port = 3000
 
 app.get('/teste', (req, res) => res
     .status(200)
-    .send({"mensagem": "'Olá!"
-}))
+    .send({mensagem: 'Olá!'}))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
 
